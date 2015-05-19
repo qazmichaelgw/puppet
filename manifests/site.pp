013eec36bcc3ccbf282adb443d6a84fffc796db8
Gaowei <qazmichaelgw@gmail.com>
2015-05-19 14:21:36 +0800

    mco test

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,5 +46,5 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include profiles::notify_a_message
+  include r10k::mcollective
 }
